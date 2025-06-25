a893c5706c12bef3e38d96d4989d0b41c2d1b3ec
BraianEspanon <braianespanon@gmail.com>
2025-06-25 19:53:18 -0300

    Update CorsConfig.java

--- src/main/java/com/braianespanon/Portfolio/security/CorsConfig.java
+++ src/main/java/com/braianespanon/Portfolio/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // aplica a todas las rutas
-                        .allowedOrigins("http://localhost:4200") // origen del frontend
+                        .allowedOrigins("https://portfolio-c4181.web.app/") // origen del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
